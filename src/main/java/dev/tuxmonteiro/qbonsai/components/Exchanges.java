ff9853dd15f2e4d7ed5b9ef6626cb827c7db98f2
tuxmonteiro <me@tuxmonteiro.dev>
2025-04-30 00:54:58 +0200

    Improve Exchanges component

--- src/main/java/dev/tuxmonteiro/qbonsai/components/Exchanges.java
+++ src/main/java/dev/tuxmonteiro/qbonsai/components/Exchanges.java
@@ -1,5 +1,6 @@
 package dev.tuxmonteiro.qbonsai.components;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.tuxmonteiro.jccxt.base.BaseExchange;
@@ -11,28 +12,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Component
 public class Exchanges {
 
-    private final JsonNode json;
-    private final JsonNode nullNode;
+    private final Map<String, Object> exchanges;
 
     public Exchanges(@Autowired ObjectMapper mapper) throws IOException {
         try (InputStream is = Objects.requireNonNull(
                 BaseExchange.class.getClassLoader().getResourceAsStream("exchanges.json"));
              BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
-            json = mapper.readTree(reader.lines().collect(Collectors.joining()));
-            nullNode = mapper.nullNode();
+            JsonNode json = mapper.readTree(reader.lines().collect(Collectors.joining()));
+            exchanges = mapper.convertValue(json, new TypeReference<ConcurrentHashMap<String, Object>>() {});
         }
         log.info("Exchanges component created");
     }
 
-    public JsonNode getExchange(String name) {
-        return Optional.ofNullable(json.get(name)).orElse(nullNode);
+    @SuppressWarnings("unchecked")
+    public Map<String, Object> getExchange(String name) {
+        return exchanges.get(name) instanceof Map<?,?> map ? (Map<String, Object>) map : Collections.emptyMap();
     }
 }
