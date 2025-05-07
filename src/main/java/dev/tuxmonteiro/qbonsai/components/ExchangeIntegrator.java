e5e75cd55ef7189f58339411a64a3ee50222ff8a
tuxmonteiro <me@tuxmonteiro.dev>
2025-05-08 00:11:58 +0200

    Refactoring Exchanges and ExchangeIntegrator

--- src/main/java/dev/tuxmonteiro/qbonsai/components/ExchangeIntegrator.java
+++ src/main/java/dev/tuxmonteiro/qbonsai/components/ExchangeIntegrator.java
@@ -1,114 +1,87 @@
 package dev.tuxmonteiro.qbonsai.components;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.tuxmonteiro.jccxt.base.types.Exchange;
 import dev.tuxmonteiro.qbonsai.services.WebSocketClientService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.web.reactive.socket.WebSocketHandler;
-import org.springframework.web.reactive.socket.client.WebSocketClient;
-import reactor.core.publisher.Mono;
 
 import java.io.IOException;
+import java.io.Serial;
 import java.net.URI;
-import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Consumer;
 
 @Slf4j
-public record ExchangeIntegrator(String name, Map<String, String> apiUrlWs, Map<String, String> subscriptionRequestTemplate,
-                                 WebSocketClientService webSocketClientService) {
+public class ExchangeIntegrator extends Exchange {
 
-    public ExchangeIntegrator(String name, Map<String, String> apiUrlWs, Map<String, String> subscriptionRequestTemplate, WebSocketClientService webSocketClientService) {
-        this.name = name;
-        this.apiUrlWs = Collections.unmodifiableMap(apiUrlWs);
-        this.subscriptionRequestTemplate = Collections.unmodifiableMap(subscriptionRequestTemplate);
+    @Serial
+    private static final long serialVersionUID = 4370324370153680134L;
+
+    private WebSocketClientService webSocketClientService;
+    private ObjectMapper mapper;
+
+    @JsonIgnore
+    public ExchangeIntegrator setWebSocketClientService(final WebSocketClientService webSocketClientService) {
         this.webSocketClientService = webSocketClientService;
+        return this;
     }
 
-    public static final class ExchangeIntegratorBuilder {
-
-        private String name = "undef";
-        private Map<String, String> apiUrlWs = Map.of();
-        private Map<String, String> subscriptionRequestTemplate = Map.of();
-        WebSocketClientService webSocketClientService = new WebSocketClientService(new WebSocketClient() {
-            @Override
-            public Mono<Void> execute(URI url, WebSocketHandler handler) {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            public Mono<Void> execute(URI url, HttpHeaders headers, WebSocketHandler handler) {
-                throw new UnsupportedOperationException();            }
-        });
-
-        private ExchangeIntegratorBuilder() {}
-
-        public ExchangeIntegratorBuilder name(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public ExchangeIntegratorBuilder apiUrlWs(Map<String, String> apiUrlWs) {
-            this.apiUrlWs = apiUrlWs;
-            return this;
-        }
-
-        public ExchangeIntegratorBuilder subscriptionRequestTemplate(Map<String, String> subscriptionRequestTemplate) {
-            this.subscriptionRequestTemplate = subscriptionRequestTemplate;
-            return this;
-        }
-
-        public ExchangeIntegratorBuilder webSocketClientService(WebSocketClientService webSocketClientService) {
-            this.webSocketClientService = webSocketClientService;
-            return this;
-        }
-
-        public ExchangeIntegrator build() {
-            return new ExchangeIntegrator(name, apiUrlWs, subscriptionRequestTemplate, webSocketClientService);
-        }
-
+    @JsonIgnore
+    public ExchangeIntegrator setObjectMapper(final ObjectMapper mapper) {
+        this.mapper = mapper;
+        return this;
     }
 
-    public static ExchangeIntegratorBuilder builder() {
-        return new ExchangeIntegratorBuilder();
-    }
-
+    @JsonIgnore
     public void connect() throws IOException {
-        String principalUri = apiUrlWs.get("principal");
-        if (principalUri != null) {
-            webSocketClientService.connect(URI.create(principalUri));
+        var apiUrlWsObj = getUrls().getApi().orElseThrow().getWs().orElseThrow();
+        if (apiUrlWsObj instanceof String apiUrlWs) {
+            webSocketClientService.connect(URI.create(apiUrlWs));
         } else {
-            throw new IOException("Missing 'principal' ws URI");
+            throw new UnsupportedOperationException("Not Implemented");
         }
     }
 
+    @JsonIgnore
     @SafeVarargs
     public final void subscribe(Consumer<String> onNext, String function, Map.Entry<String, String>... functionParams) {
-        var requestTemplate = subscriptionRequestTemplate.get(function);
+        var requestTemplate = getRequestTemplate(function);
         String sendMessage = processTemplate(requestTemplate, functionParams);
         log.info("Sending message {}", sendMessage);
 
         webSocketClientService.send(sendMessage, onNext);
     }
 
+    @JsonIgnore
     public void disconnect() {
         webSocketClientService.disconnect();
     }
 
+    @JsonIgnore
+    private String getRequestTemplate(String function) {
+        var functionsWsReq = Optional.ofNullable(getFunctionsWsReq().get(getId() + "." + function)).orElseThrow();
+        var requestTemplate = functionsWsReq.getRequestTemplate();
+        var encoded = requestTemplate.getEncoded();
+        Object requestTemplateTemplateObj = encoded ? "" : requestTemplate.getTemplate();
+
+        try {
+            return mapper.writer().writeValueAsString(requestTemplateTemplateObj);
+        } catch (JsonProcessingException e) {
+            log.error("Object to json problem", e);
+            return "";
+        }
+    }
+
+    @JsonIgnore
     @SafeVarargs
     private final String processTemplate(String template, Map.Entry<String, String>... entries) {
         for (Map.Entry<String, String> entry : entries) {
             template = template.replaceAll("[{]" + entry.getKey() + "[}]", entry.getValue());
         }
         return template;
-    }
-
-
-    public String getWebSocketUrl(String exchange) {
-        return getWebSocketUrl(exchange, "principal");
-    }
-
-    public String getWebSocketUrl(String exchangeName, String subPath) {
-        return apiUrlWs().get(subPath);
     }
 
 }
--- src/main/java/dev/tuxmonteiro/qbonsai/components/Exchanges.java
+++ src/main/java/dev/tuxmonteiro/qbonsai/components/Exchanges.java
@@ -1,6 +1,5 @@
 package dev.tuxmonteiro.qbonsai.components;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.tuxmonteiro.jccxt.base.types.Exchange;
@@ -14,89 +13,33 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Component
 public class Exchanges {
 
-    private final ObjectMapper mapper;
-    private final Map<String, Exchange> exchangesDTO = new ConcurrentHashMap<>();
-    private final Map<String, ExchangeIntegrator> exchanges = new ConcurrentHashMap<>();
-    private final WebSocketClientService webSocketClientService;
+    private final Map<String, ExchangeIntegrator> exchanges = Collections.synchronizedMap(new HashMap<>());
 
     @Autowired
     public Exchanges(ObjectMapper mapper, WebSocketClientService webSocketClientService) throws IOException {
-        this.mapper = mapper;
-        this.webSocketClientService = webSocketClientService;
         try (
                 InputStream is = Objects.requireNonNull(Exchange.class.getClassLoader().getResourceAsStream("exchanges.json"));
                 BufferedReader reader = new BufferedReader(new InputStreamReader(is))
         ) {
-            JsonNode json = this.mapper.readTree(reader.lines().collect(Collectors.joining()));
+            JsonNode json = mapper.readTree(reader.lines().collect(Collectors.joining()));
             json.fieldNames().forEachRemaining(ex ->
-                exchangesDTO.put(ex, this.mapper.convertValue(json.get(ex), Exchange.class))
+                this.exchanges.put(ex, mapper.convertValue(json.get(ex), ExchangeIntegrator.class)
+                        .setWebSocketClientService(webSocketClientService)
+                        .setObjectMapper(mapper))
             );
         }
 
         log.info("Exchanges component created");
     }
 
-    public synchronized ExchangeIntegrator getExchange(String name) {
-        ExchangeIntegrator exchangeIntegrator = exchanges.get(name);
-        if (exchangeIntegrator != null) {
-            return exchangeIntegrator;
-        }
-
-        final Map<String, Object> exchangeFromJson;
-        Optional.ofNullable(exchangesDTO.get(name))
+    public ExchangeIntegrator getExchange(String name) {
+        return Optional.ofNullable(exchanges.get(name))
                 .orElseThrow(() -> new IllegalArgumentException("Exchange " + name + " not found"));
-
-        String apiWs = exchangesDTO.get("bitstamp").getUrls().getApi().orElseThrow().getWs().orElseThrow().toString();
-
-        String function = "watchTrades";
-        var subscriptionRequestTemplate = Map.entry(function, getRequestTemplate(name, function));
-
-        exchangeIntegrator = ExchangeIntegrator.builder()
-                .name(name)
-                .apiUrlWs(Map.of("principal", apiWs))
-                .subscriptionRequestTemplate(Map.ofEntries(subscriptionRequestTemplate))
-                .webSocketClientService(webSocketClientService)
-                .build();
-
-        log.info("Creating exchange {}", name);
-
-        exchanges.put(name, exchangeIntegrator);
-
-        return exchangeIntegrator;
     }
-
-    @SuppressWarnings("unchecked")
-    private Map<String, Object> getObjectMap(Map<String, Object> obj, String property) {
-        return (obj instanceof Map<String, Object> map) ?
-            (Map<String, Object>) Optional.ofNullable(map.get(property)).orElse(Collections.emptyMap()) : Collections.emptyMap();
-    }
-
-    private String extractString(Map<String, Object> obj, String property) {
-        return (obj instanceof Map<String, Object> map) ?
-            Optional.ofNullable(map.get(property)).orElse("").toString() : "";
-    }
-
-    private String getRequestTemplate(String exchange_name, String function) {
-        var exchange = Optional.ofNullable(exchangesDTO.get(exchange_name)).orElseThrow();
-        var watchTradesFunction = Optional.ofNullable(exchange.getFunctionsWsReq().get(exchange_name + "." + function)).orElseThrow();
-
-        var requestTemplate = watchTradesFunction.getRequestTemplate();
-        var encoded = requestTemplate.getEncoded();
-        Object requestTemplateTemplateObj = encoded ? "" : requestTemplate.getTemplate();
-
-        try {
-            return mapper.writer().writeValueAsString(requestTemplateTemplateObj);
-        } catch (JsonProcessingException e) {
-            log.error("Object to json problem", e);
-            return "";
-        }
-    }
-
 }
